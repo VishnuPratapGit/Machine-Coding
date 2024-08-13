f3a06a324c3017f38b7ec5cdc5ed2973c67114e8
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-08-13 15:32:23 +0530

    add folder and file functionality

--- file_explorer/src/components/FileExplorer.jsx
+++ file_explorer/src/components/FileExplorer.jsx
@@ -1,26 +1,79 @@
+import { FolderPlus, FilePlus2 } from 'lucide-react';
 import { useState } from "react";
+import "./fileExplorer.css";
 
-const FileExplorer = ({folderData}) => {
-    const[open, setOpen] = useState(false);
-    
+const FileExplorer = ({ folderData }) => {
+    const [open, setOpen] = useState(false);
+    const [folderName, setFolderName] = useState("");
+    const [isFolderEdit, setIsFolderEdit] = useState(false);
+    const [isFileEdit, setIsFileEdit] = useState(false);
+    console.log(folderData);
+
+    function folderEdit() {
+        setIsFolderEdit(!isFolderEdit);
+        setIsFileEdit(false)
+    }
+    function fileEdit() {
+        setIsFileEdit(!isFileEdit)
+        setIsFolderEdit(false);
+    }
+
+    function addData(e) {
+        e.preventDefault();
+        let newData = {
+            name: folderName,
+            type: isFolderEdit ? "folder" : "file",
+            children: []
+        }
+        folderData.children.push(newData);
+        setFolderName("")
+        setIsFolderEdit(false);
+        setIsFileEdit(false);
+    }
+
     return (
         <div>
             <div className="type__name">
-                {(folderData.type === "folder") ? (
-                    <div onClick={() => setOpen(!open)}>
-                        {open ? <div>📂 {folderData.name}</div>: <div>📁 {folderData.name}</div>}
+                <div onClick={() => setOpen(!open)}>
+                    {folderData.type === "folder" ? (open ? "📂" : "📁") : "📄"}{" "}
+                    {folderData.name}
+                </div>
+                {folderData.type === "folder" && open && (
+                    <div className="create__span">
+                        <abbr onClick={folderEdit} title="create folder">
+                            <FolderPlus size={20} color="grey" strokeWidth={1} />
+                        </abbr>
+                        <abbr onClick={fileEdit} title="create file">
+                            <FilePlus2 size={20} color="grey" strokeWidth={1} />
+                        </abbr>
                     </div>
-                    ) : (
-                    <div>📄 {folderData.name}</div>
-                ) }
+                )}
             </div>
-            {open && <div className="childrens">
-                {folderData.children.map(data => (
-                    <FileExplorer key={data.name} folderData={data}/>
-                ))}
-            </div>}
+
+            {open && (
+                <div>
+                    {(isFolderEdit || isFileEdit) &&
+                        <form onSubmit={(e) => addData(e)}>
+                            <input
+                                type="text"
+                                value={folderName}
+                                onChange={(e) => setFolderName(e.target.value)}
+                            />
+                        </form>
+                    }
+                    <div className="childrens">
+                        {folderData.children.sort((a, b) => {
+                            if (a.type > b.type) return -1;
+                            else if (a.type < b.type) return 1;
+                            else return 0;
+                        }).map((data) => (
+                            <FileExplorer key={data.name} folderData={data} />
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default FileExplorer;
